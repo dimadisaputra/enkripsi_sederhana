0bbb5798589a5ca47bb5bf4bb1cc8bf427d62877
Dimas Adi Saputra <dimadisaputra@gmail.com>
2023-05-04 05:38:02 +0700

    Second Commit

--- enkripsi.py
+++ enkripsi.py
@@ -12,3 +12,20 @@ def fungsi_enkripsi(inputan):
     enkripsi_str = "".join(enkripsi_karakter)
 
     return enkripsi_str
+
+def fungsi_dekripsi(inputan):
+    # konversi inputan ke string dan ambil setiap karakternya
+    inputan_str = str(inputan)
+    
+    # split hasil enkripsi ke dalam list dengan panjang 4 digit
+    karakter = [inputan_str[i:i+4] for i in range(0, len(inputan_str), 4)]
+    
+    # dekripsi setiap karakter
+    dekripsi_karakter = []
+    for kar in karakter:
+        dekripsi_karakter.append(chr(int(kar) // 69))
+
+    # gabungkan hasil dekripsi ke dalam string
+    dekripsi_str = "".join(dekripsi_karakter)
+
+    return dekripsi_str
--- main.py
+++ main.py
@@ -6,7 +6,9 @@ inputan_user = input("Masukkan inputan Anda: ")
 
 # enkripsi inputan
 hasil_enkripsi = enkripsi.fungsi_enkripsi(inputan_user)
+hasil_dekripsi = enkripsi.fungsi_dekripsi(hasil_enkripsi)
 
 # cetak inputan dan hasil enkripsi ke layar
 print("Inputan Anda: ", inputan_user)
 print("Hasil Enkripsi: ", hasil_enkripsi)
+print("Hasil Dekripsi: ", hasil_dekripsi)
